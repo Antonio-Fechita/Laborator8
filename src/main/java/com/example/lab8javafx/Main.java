ec2ee7ee7b13cd51cc43bf52e7edadc6d6714f80
Antonio Fechita <antonio.fechita@yahoo.com>
2022-04-30 22:57:03 +0300

    2d map work

--- src/main/java/com/example/lab8javafx/Main.java
+++ src/main/java/com/example/lab8javafx/Main.java
@@ -24,10 +24,18 @@ public class Main {
         continentDAO.createContinent("America de Nord");
         continentDAO.createContinent("America de Sud");
 
+        countryDAO.createCountry("fr", "Franta", continentDAO.findByName("Europa"));
+        countryDAO.createCountry("br","Brazilia",continentDAO.findByName("America de Sud"));
+        countryDAO.createCountry("ca","Canada", continentDAO.findByName("America de Nord"));
+        countryDAO.createCountry("av","Australia de Vest",continentDAO.findByName("Australia"));
         countryDAO.createCountry("ja","Japonia",continentDAO.findByName("Asia"));
         countryDAO.createCountry("ro","Romania",continentDAO.findByName("Europa"));
         countryDAO.createCountry("ni","Nigeria",continentDAO.findByName("Africa"));
 
+        cityDAO.createCity("Paris",true,48.864716,2.349014,countryDAO.findByName("Franta"));
+        cityDAO.createCity("Brasilia",true,-15.793889,	-47.882778,countryDAO.findByName("Brazilia"));
+        cityDAO.createCity("Ottawa",true,45.334904,-75.724098,countryDAO.findByName("Canada"));
+        cityDAO.createCity("Perth",true,-31.953512,115.857048,countryDAO.findByName("Australia de Vest"));
         cityDAO.createCity("Abuja",true,9.066667,7.483333,countryDAO.findByName("Nigeria"));
         cityDAO.createCity("Iasi",false,47.151726,27.587914,countryDAO.findByName("Romania"));
         cityDAO.createCity("Tokyo",true,35.652832,139.839478,countryDAO.findByName("Japonia"));
---
+++ src/main/java/com/example/lab8javafx/Utility/Pair.java
@@ -0,0 +1,54 @@
+package com.example.lab8javafx.Utility;
+
+import java.util.Objects;
+
+public class Pair<T,K> {
+
+    private T first;
+    private K second;
+
+    public Pair(T first, K second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public Pair() {
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public K getSecond() {
+        return second;
+    }
+
+    public void setSecond(K second) {
+        this.second = second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(first, pair.first) && Objects.equals(second, pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "first=" + first +
+                ", second=" + second +
+                '}';
+    }
+}
